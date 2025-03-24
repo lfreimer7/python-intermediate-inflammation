42a9dc8b2f633356f9c3092cd7b859decc92709b
unknown <lfreimer2@gmail.com>
2025-03-24 12:14:32 -0500

    Spelling fix

--- inflammation/models.py
+++ inflammation/models.py
@@ -19,7 +19,7 @@ def load_csv(filename):
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2d inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array."""
     return np.mean(data, axis=0)
 
 
@@ -29,6 +29,6 @@ def daily_max(data):
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2d inflammation data array."""
+    """Calculate the daily min of a 2D inflammation data array."""
     return np.min(data, axis=0)
 
